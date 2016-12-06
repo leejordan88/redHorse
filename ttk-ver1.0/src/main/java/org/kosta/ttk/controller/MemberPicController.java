a05334dc7760e66bc49c272a34ef7dfa09cfba8b
dudwn <dudwn@192.168.0.173>
2016-12-06 16:44:24 +0900

    ㅇㅈ

--- ttk-ver1.0/src/main/java/org/kosta/ttk/controller/MemberPicController.java
+++ ttk-ver1.0/src/main/java/org/kosta/ttk/controller/MemberPicController.java
@@ -20,7 +20,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class MemberPicController {
 	@Resource
-		private MemberPicService memberPicService;
+	private MemberPicService memberPicService;
 	
 	private String uploadPath;
 	/**
