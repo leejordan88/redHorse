7cafdd704a0e6e3f5541208db97db7c42fda907b
leejoonsung <leejs198@gmail.com>
2016-12-06 12:35:51 +0900

    merge with 1.4

--- ttk-ver1.0/src/main/java/org/kosta/ttk/model/service/MemberService.java
+++ ttk-ver1.0/src/main/java/org/kosta/ttk/model/service/MemberService.java
@@ -1,5 +1,7 @@
 package org.kosta.ttk.model.service;
 
+import java.util.List;
+
 import org.kosta.ttk.model.vo.MemberVO;
 
 public interface MemberService {
@@ -13,4 +15,8 @@ public interface MemberService {
 	
 	void registerMember(MemberVO memberVO);
 	void updateDelete(MemberVO vo);
+
+	List<MemberVO> searchMemberByOption(String str);
+
+	int idcheck(String id);
 }
